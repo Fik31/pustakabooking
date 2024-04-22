be8c07992f2c56232519789ce37bdc202d6489a1
fik31 <fikrian.famz31@gmail.com>
2024-04-22 19:53:21 +0700

    pengambilan data dari controller

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -1,13 +1,13 @@
 <?php
-defined('BASEPATH') or exit ('no direct script access allowed');
+defined('BASEPATH') or exit ('No direct script access allowed');
+
 class Web extends CI_Controller
 {
     function __construct()
     {
         parent::__construct();
-        // this->load->helper('url');
-
     }
+
     public function index()
     {
         $data['judul'] = "Halaman Depan";
@@ -19,6 +19,12 @@ class Web extends CI_Controller
     public function about()
     {
         $data['judul'] = "Halaman About";
+        $data['nama_depan'] = "Fikrian";
+        $data['nama_belakang'] = "Nur";
+        $data['alamat'] = "Jakarta";
+        $data['tempat_lahir'] = "Bekasi";
+        $data['olahraga_favorit'] = array("Basket", "Catur");
+
         $this->load->view('v_header', $data);
         $this->load->view('v_about', $data);
         $this->load->view('v_footer', $data);
--- application/views/v_about.php
+++ application/views/v_about.php
@@ -1,25 +1,23 @@
 <section>
-    <h1>
-        <?php echo $judul ?>
-    </h1>
+    <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Fikrian</li>
-        <li>Nama Belakang : Nur</li>
+        <li>Nama Depan : <?php echo $nama_depan ?></li>
+        <li>Nama Belakang : <?php echo $nama_belakang ?></li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li>Jakarta</li>
+        <li><?php echo $alamat ?></li>
     </ul>
-
     <h4>Tempat Lahir</h4>
     <ul type="none">
-        <li>Bekasi</li>
+        <li><?php echo $tempat_lahir ?></li>
     </ul>
     <h4>Olah Raga Favorit</h4>
     <ul type="square">
-        <li>Basket</li>
-        <li>Catur</li>
+        <?php foreach ($olahraga_favorit as $olahraga) { ?>
+            <li><?php echo $olahraga ?></li>
+        <?php } ?>
     </ul>
 </section>
